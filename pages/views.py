b79ccd4c25b93e51639dbb5987ccf1771962aabe
Marko <marko_poikkimaki@hotmail.com>
2020-09-14 19:02:14 +0200

    get pages

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Page
 
 
-def index(request):
-    return render(request, 'pages/page.html')
+def index(request, pagename):
+    pagename = '/' + pagename
+    print('page: ' + repr(pagename))
+    page = Page.objects.get(permalink=pagename)
+
+    context = {
+        'title': page.title,
+        'content': page.bodytext,
+        'last_update': page.update_date
+    }
+    return render(request, 'pages/page.html', context)
 
 
